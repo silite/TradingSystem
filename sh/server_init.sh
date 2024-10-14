9cb1821c0ea9ad2a3ae8ce5dddef3ab26096495a
silite <32181762+silite@users.noreply.github.com>
2024-10-14 17:36:50 +0800

    feat: init

---
+++ sh/server_init.sh
@@ -0,0 +1,5 @@
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup toolchain install nightly
+rustup default
+sudo apt install build-essential
+sudo apt install libssl-dev pkg-config
